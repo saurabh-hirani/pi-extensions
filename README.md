6eb3eccb364850089d84e2c9a3ec953728e14473
Arjun Mahishi <arjun.mahishi@gmail.com>
2026-04-12 18:28:26 +0530

    Add install script and update README

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ who want to learn pi together.
 There are no specific contribution guidelines as the project is in its nascent
 stages and a learning experiment.
 
+### Installing an extension
+
+Run `./install.sh`. This will bring up a TUI which will let you toggle extensions on and off.
+
+Toggle on = Symlink the extension file to your `~/.pi/agent/extensions` directory.
+Toggle off = Remove the symlink from your `~/.pi/agent/extensions` directory.
+
+The purpose of this script is to make it easy to install/uninstall extensions
+from here that you have not authored.
+
 ### Saurabh Hirani
 
 1. [pi vim extension](./saurabh-hirani/.pi/agent/extensions/pi-vim.ts)
---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -e
+
+REPO_DIR="$(git rev-parse --show-toplevel)"
+TARGET_DIR="$HOME/.pi/agent/extensions"
+
+# Write extensions to a file so we can index by line number
+EXT_FILE=$(mktemp)
+find "$REPO_DIR" -path "*/extensions/*.ts" -type f | sort > "$EXT_FILE"
+count=$(wc -l < "$EXT_FILE" | tr -d ' ')
+trap "rm -f $EXT_FILE" EXIT
+
+if [ "$count" -eq 0 ]; then
+    echo "No extensions found"
+    exit 0
+fi
+
+# Colors for authors
+COLORS=("\033[31m" "\033[32m" "\033[33m" "\033[34m" "\033[35m" "\033[36m")
+RESET="\033[0m"
+
+# Assign a color to each unique author
+author_color() {
+    author="$1"
+    # Hash the author name to pick a consistent color
+    idx=$(echo "$author" | cksum | awk '{print $1 % 6}')
+    echo "${COLORS[$idx]}"
+}
+
+render() {
+    clear
+    echo "=== Extensions ==="
+    echo ""
+    i=1
+    while IFS= read -r ext; do
+        filename=$(basename "$ext" .ts)
+        author=$(echo "$ext" | sed "s|$REPO_DIR/||" | cut -d'/' -f1)
+        color=$(author_color "$author")
+
+        if [ -L "$TARGET_DIR/$(basename "$ext")" ]; then
+            printf "  [x] %s) ${color}%s${RESET} • %s\n" "$i" "$author" "$filename"
+        else
+            printf "  [ ] %s) ${color}%s${RESET} • %s\n" "$i" "$author" "$filename"
+        fi
+        i=$((i + 1))
+    done < "$EXT_FILE"
+    echo ""
+    printf "1-$count: toggle | Enter: save & quit\n"
+}
+
+toggle() {
+    ext=$(sed -n "${1}p" "$EXT_FILE")
+    filename=$(basename "$ext")
+    if [ -L "$TARGET_DIR/$filename" ]; then
+        rm "$TARGET_DIR/$filename"
+    else
+        ln -s "$ext" "$TARGET_DIR/$filename"
+    fi
+}
+
+while true; do
+    render
+    read -rn1 key
+    case $key in
+        ''|q|Q) echo ""; exit 0 ;;
+        [1-9])
+            if [ "$key" -le "$count" ]; then
+                toggle "$key"
+            fi
+            ;;
+    esac
+done
